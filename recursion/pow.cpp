88ca9e743147cb6e30722e9d4f29a1c1bb368bd3
Tanveer Raza <tanveerRaza.tr12@gmail.com>
2024-09-19 00:38:22 +0530

    some new code

---
+++ recursion/pow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int pow(int a, int b)
+{
+    if (b==1) return 1;
+    return a * pow(b/2,b/2);
+}
+int main()
+{
+    int a;
+    cout << "Enter base: " << endl;
+    cin >> a;
+     int b;
+    cout << "Enter power: " << endl;
+    cin >> b;
+    cout<<pow(a,b);
+}
