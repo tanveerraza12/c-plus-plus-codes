c182c50b4f58e4343ae826664d78315a22762998
Tanveer Raza <tanveerRaza.tr12@gmail.com>
2024-09-26 22:28:26 +0530

    new

---
+++ Array/2dArray/sum.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+// this program add 2 matrix
+int main()
+{
+    int arr1[2][3];
+    cout << "enter no for sum" << endl;
+    for (int i = 0; i < 2; i++) // row input
+    {
+        for (int j = 0; j < 3; j++) // col input
+        {
+            cout << "enter no. :";
+            cin >> arr1[i][j];
+        }
+        cout << endl;
+    }
+    int arr2[2][3];
+    cout << "enter no for 2 array sum" << endl;
+    for (int i = 0; i < 2; i++) // row input
+    {
+        for (int j = 0; j < 3; j++) // col input
+        {
+            cout << "enter no. :";
+            cin >> arr2[i][j];
+        }
+        cout << endl;
+    }
+    int sum[2][3];
+    sum[2][3] = arr1[2][3] + arr2[2][3];
+
+    for (int i=0; i <2; i++)//row output
+    {
+         for (int j=0; j<3; j++)//col output
+    {
+         cout << sum[i][j]<<" ";
+
+    }
+    cout << endl;
+    }
+}
+
+// input takes, but output is wrong
